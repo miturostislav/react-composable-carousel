8e509d09e7ad63e23cd930f88cdcb4190b9577eb
Mitu Rostislav <rmitu@tacitknowledge.com>
2019-04-29 14:51:32 +0300

    Add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+  extends: ['eslint:recommended', 'prettier'],
+  parser: 'babel-eslint',
+  env: {
+    browser: true,
+    es6: true,
+  },
+};
---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+/* eslint-disable */
+module.exports = {
+  tabWidth: 2,
+  singleQuote: true,
+  jsxBracketSameLine: false,
+  trailingComma: 'es5',
+  arrowParens: 'avoid',
+};
